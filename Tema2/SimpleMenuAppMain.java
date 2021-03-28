a1350850bc315322ce3bc12c00d6e10d43f1895f
AndreeaMariaDraghici <draghici108@gmail.com>
2021-03-28 18:28:12 +0300

    Create SimpleMenuAppMain.java

---
+++ Tema2/SimpleMenuAppMain.java
@@ -0,0 +1,11 @@
+package tema2;
+
+public class SimpleMenuAppMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SimpleMenu menu = new SimpleMenu();
+		menu.Run();	
+	}
+
+}
