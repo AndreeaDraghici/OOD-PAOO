2a1012dfbe8064a21d73d64d41ef0446554d3d98
AndreeaMariaDraghici <draghici108@gmail.com>
2021-04-04 21:58:59 +0300

    Create MainReusableMenu.java

---
+++ Tema3/src/reusablemenu/sample/MainReusableMenu.java
@@ -0,0 +1,10 @@
+package reusablemenu.sample;
+
+public class MainReusableMenu {
+
+	public static void main(String[] args) {
+		ApplicationMenu menu = new ApplicationMenu();
+		menu.load();
+		menu.execute();
+	}
+}
