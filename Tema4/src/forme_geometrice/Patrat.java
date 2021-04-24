86a4687e877595bb7efd1389d0940983fd412559
AndreeaMariaDraghici <draghici108@gmail.com>
2021-04-24 18:16:16 +0300

    Create Patrat.java

---
+++ Tema4/src/forme_geometrice/Patrat.java
@@ -0,0 +1,20 @@
+package forme_geometrice;
+
+import abstractions.IGraphicElement;
+
+public class Patrat implements IGraphicElement{
+	private double latura=0;
+	
+	public Patrat(double _latura){
+		this.latura = _latura;
+	}
+	
+	public double getLatura() {return latura;}
+	public void setLatura(double _latura) {this.latura = _latura;}
+	
+	@Override
+	 public void draw() {
+	        System.out.println("Patrat:latura=" + latura);
+	 }
+	
+}
