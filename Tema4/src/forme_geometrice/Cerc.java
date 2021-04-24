9707091621eea06355e1914693e685b51213fab4
AndreeaMariaDraghici <draghici108@gmail.com>
2021-04-24 18:16:12 +0300

    Create Cerc.java

---
+++ Tema4/src/forme_geometrice/Cerc.java
@@ -0,0 +1,32 @@
+package forme_geometrice;
+
+import abstractions.IGraphicElement;
+
+public class Cerc implements IGraphicElement {
+	
+	private double Ox=0;
+	private double Oy=0;
+	private double raza=0;
+	
+	public Cerc(double _Ox, double _Oy, double _raza){
+		this.Ox = _Ox;
+		this.Oy = _Oy;
+		this.raza = _raza;
+	}
+	
+	public double getOx() {return Ox;}
+	public void setOx(double _Ox) {this.Ox = _Ox;}
+
+	public double getOy() {return Oy;}
+	public void setOy(double _Oy) {this.Oy = _Oy;}
+
+	public double getRaza() {return raza;}
+	public void setRaza(double _raza) {this.raza = _raza;}
+
+	@Override
+	public void draw(){
+		System.out.println("Cerc:Ox= " + Ox + ", Oy= " + Oy + ", Raza= " + raza);
+	}
+
+}
+
