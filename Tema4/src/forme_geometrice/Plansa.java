108f3abef7ccf9fdf539dbeb1d549cc08f98e12e
AndreeaMariaDraghici <draghici108@gmail.com>
2021-04-24 18:16:17 +0300

    Create Plansa.java

---
+++ Tema4/src/forme_geometrice/Plansa.java
@@ -0,0 +1,29 @@
+package forme_geometrice;
+import java.util.ArrayList;
+import abstractions.*;
+
+public class Plansa implements IGraphicElement {
+        
+	private ArrayList<IGraphicElement>elements=new ArrayList<IGraphicElement>();
+	
+	public void addElement(IGraphicElement element){
+		elements.add(element);
+    }
+	
+	public void removeElement(int id) {
+		elements.remove(id);
+	}
+	
+	public IGraphicElement getElement(int id){
+		return elements.get(id);
+	}
+	
+	public IGraphicElement setElement(int id,IGraphicElement element){
+		return elements.set(id,element);
+	}
+
+	@Override
+	public void draw() {
+		elements.forEach((element)->{element.draw();});
+	}
+}
