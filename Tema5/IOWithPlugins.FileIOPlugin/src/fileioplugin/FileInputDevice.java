1bedbf544c7f628c0d3b2148c59f7a7e835fedfd
Andreea-Maria Drăghici <72825756+AndreeaDraghici@users.noreply.github.com>
2021-05-01 10:47:31 +0300

    Update FileInputDevice.java

--- Tema5/IOWithPlugins.FileIOPlugin/src/fileioplugin/FileInputDevice.java
+++ Tema5/IOWithPlugins.FileIOPlugin/src/fileioplugin/FileInputDevice.java
@@ -12,30 +12,33 @@ public class FileInputDevice implements IInputDevice {
 	public String ReadString() {
 		FileInputStream file = null;
 		String string = "";
-		@SuppressWarnings("resource")
-		Scanner scanner = new Scanner(file);
 		try {
-            file = new FileInputStream("input");
-            string += scanner.nextLine();
-            string+="\n";
-        }catch(FileNotFoundException e)
-		{
-        	System.out.println("Eroare la deschiderea fisierului " + e.getMessage());
-		}
-        catch(Exception e){
-            System.out.println("Eroare procesare fisier " + e.getMessage());
-            e.printStackTrace();
-        }
+	            file = new FileInputStream("input");
+	            @SuppressWarnings("resource")
+				Scanner scanner = new Scanner(file);
+	            string = scanner.nextLine();
+            
+            }catch(FileNotFoundException e){
+        	
+        	 System.out.println("Eroare la deschiderea fisierului " + e.getMessage());
+        	 
+	     	}catch(Exception e){
+			
+	            System.out.println("Eroare procesare fisier " + e.getMessage());
+	            e.printStackTrace();
+            }
 		if(file != null)
 		{
 			try {
+				
 				file.close();
-			}
-			catch(IOException e)
-			{
+				
+			}catch(IOException e){
+				
 				System.out.println("Fisierul nu este deschis " + e.getMessage());
 			}
 		}
 		return string;
 	}
 }
+
