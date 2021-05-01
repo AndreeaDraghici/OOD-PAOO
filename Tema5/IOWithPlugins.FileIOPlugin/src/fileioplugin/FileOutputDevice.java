51ee4171e23bee47b16b445f9838e88bcb009a00
Andreea-Maria Drăghici <72825756+AndreeaDraghici@users.noreply.github.com>
2021-05-01 12:58:39 +0300

    Update FileOutputDevice.java

--- Tema5/IOWithPlugins.FileIOPlugin/src/fileioplugin/FileOutputDevice.java
+++ Tema5/IOWithPlugins.FileIOPlugin/src/fileioplugin/FileOutputDevice.java
@@ -7,38 +7,43 @@ import java.io.PrintWriter;
 import iowithplugins.abstractions.IOutputDevice;
 
 public class FileOutputDevice implements IOutputDevice{
+	@SuppressWarnings("hiding")
 	@Override
 	public void WriteString(String s) {
 		FileOutputStream fileoutput = null;
 		
 		 try {
-	                fileoutput = new FileOutputStream("output");
-	                @SuppressWarnings("resource")
-			PrintWriter writer = new PrintWriter(fileoutput);
-	                writer.print(s);
-	                writer.flush();
+		          fileoutput = new FileOutputStream("output");
+		          @SuppressWarnings("resource")
+				  PrintWriter writer = new PrintWriter(fileoutput);
+		          writer.print(s);
+		          writer.flush();
 	         
-		     }catch(FileNotFoundException e){
+		      }catch(FileNotFoundException e){
 		    	 
-			   System.out.println("Eroare la deschiderea fisierului " + e.getMessage());
+				   System.out.println("Eroare la deschiderea fisierului " + e.getMessage());
+				   e.printStackTrace();
 			   
-		     }catch (Exception e){
+		      }catch (IOException e){
 		    	 
-	                   System.out.println("Eroare la procesarea fisierului " + e.getMessage());
-	                   e.printStackTrace();
-	             }
+		            System.out.println("Eroare la procesarea fisierului " + e.getMessage());
+		            e.printStackTrace();
+	            
+	          } catch(Exception e) {
+	        	 
+			       System.out.println("Eroare neasteptata");
+		      }
+		      finally {
 		 
-		 if(fileoutput != null)
-		 {
-			 try {
-				 
-				  fileoutput.close();
-				 
-			     }catch(IOException e){
-				 
-				 System.out.println("Fisierul nu este deschis "+e.getMessage());
-			 }
-		 }
-        }
+					 if(fileoutput != null)
+					 {
+						 try {
+							   fileoutput.close();
+						     }catch(IOException e){
+						    	 
+							 System.out.println("Fisierul nu este deschis "+e.getMessage());
+						 }
+					 }
+		     }
+    }
 }
-
