2d99a0ab8e2b08c16c2b7293e3261ab85f1f3566
Andreea-Maria Drăghici <72825756+AndreeaDraghici@users.noreply.github.com>
2021-05-01 12:59:12 +0300

    Update FileInputDevice.java

--- Tema5/IOWithPlugins.FileIOPlugin/src/fileioplugin/FileInputDevice.java
+++ Tema5/IOWithPlugins.FileIOPlugin/src/fileioplugin/FileInputDevice.java
@@ -8,36 +8,43 @@ import java.util.Scanner;
 import iowithplugins.abstractions.IInputDevice;
 
 public class FileInputDevice implements IInputDevice {
+	@SuppressWarnings("hiding")
 	@Override
 	public String ReadString() {
 		FileInputStream file = null;
 		String string = "";
 		try {
-	              file = new FileInputStream("input");
-	              @SuppressWarnings("resource")
-		      Scanner scanner = new Scanner(file);
-	              string = scanner.nextLine();
-            
-                   }catch(FileNotFoundException e){
+	            file = new FileInputStream("input");
+	            @SuppressWarnings("resource")
+				Scanner scanner = new Scanner(file);
+	            string = scanner.nextLine();
+	        
+             }catch(FileNotFoundException e){
         	
-        	           System.out.println("Eroare la deschiderea fisierului " + e.getMessage());
+	        	 System.out.println("Fisierul nu exista " + e.getMessage());
+	        	 e.printStackTrace();
         	 
-	     	   }catch(Exception e){
-			
-	                   System.out.println("Eroare procesare fisier " + e.getMessage());
-	                   e.printStackTrace();
-            }
-		if(file != null)
-		{
-			try {
+	     	 }catch(IOException e){
+			 
+	             System.out.println("Fisierul nu poate fi citit" + e.getMessage());
+	             e.printStackTrace();
+             }
+		     catch(Exception e) {
+			     System.out.println("Eroare neasteptata");
+		     }
+			finally {
 				
-				file.close();
-				
-			   }catch(IOException e){
-				
-				System.out.println("Fisierul nu este deschis " + e.getMessage());
-			}
-		}
+					if(file != null)
+					{
+						 try 
+						 {
+						   file.close();	
+						 }catch(IOException e)
+						  {
+						   System.out.println("Fisierul nu este deschis " + e.getMessage());
+						  }
+					}
+		          }  
 		return string;
 	}
 }
