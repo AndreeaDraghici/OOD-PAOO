3847b57adedff6b7a130ad94b7c43d87d32126f5
Andreea-Maria Drăghici <72825756+AndreeaDraghici@users.noreply.github.com>
2021-05-01 10:46:58 +0300

    Update FileOutputDevice.java

--- Tema5/IOWithPlugins.FileIOPlugin/src/fileioplugin/FileOutputDevice.java
+++ Tema5/IOWithPlugins.FileIOPlugin/src/fileioplugin/FileOutputDevice.java
@@ -4,7 +4,6 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
-
 import iowithplugins.abstractions.IOutputDevice;
 
 public class FileOutputDevice implements IOutputDevice{
@@ -13,30 +12,33 @@ public class FileOutputDevice implements IOutputDevice{
 		FileOutputStream fileoutput = null;
 		
 		 try {
-	            fileoutput = new FileOutputStream("output");
+	          fileoutput = new FileOutputStream("output");
 	          @SuppressWarnings("resource")
 			  PrintWriter writer = new PrintWriter(fileoutput);
 	          writer.print(s);
 	          writer.flush();
 	         
-		 }catch(FileNotFoundException e)
-		 {
-			 System.out.println("Eroare la deschiderea fisierului " + e.getMessage());
-		 }
-	          
-	        catch (Exception ex){
-	            System.out.println("Eroare: " + ex.getMessage());
-	        }
+		     }catch(FileNotFoundException e){
+		    	 
+			   System.out.println("Eroare la deschiderea fisierului " + e.getMessage());
+			   
+		     }catch (Exception e){
+		    	 
+	            System.out.println("Eroare la procesarea fisierului " + e.getMessage());
+	            e.printStackTrace();
+	         }
+		 
 		 if(fileoutput != null)
 		 {
 			 try {
-				 fileoutput.close();
-			 }
-			 catch(IOException ex)
-			 {
-				 System.out.println("Mesaj: "+ex.getMessage());
+				 
+				  fileoutput.close();
+				 
+			    }catch(IOException e){
+				 
+				 System.out.println("Fisierul nu este deschis "+e.getMessage());
 			 }
 		 }
+    }
+}
 
-}
-}
